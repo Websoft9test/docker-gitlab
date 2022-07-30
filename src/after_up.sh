ec59fbc88ed059a13b76890629a9e291efe38113
qiaofeng1227 <76487013@qq.com>
2022-07-30 09:37:19 +0800

    Update after_up.sh

--- src/after_up.sh
+++ src/after_up.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+gitlab_pwd=$(sudo docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password)
+sudo sed -i "s/APP_PASSWOR.*/APP_PASSWOR=$gitlab_pwd/g" /data/apps/gitlab/.env
+sudo sed -i "s/APP_PASSWOR.*/APP_PASSWOR=$gitlab_pwd/g" /credentials/password.txt
